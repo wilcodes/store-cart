20b52105b5be369f4ff6f301adcb5144dce10b38
Wilfredo R Colina <wrca26@gmail.com>
2022-11-23 13:22:17 -0600

    Logic for php cart stablished

--- server/insert.php
+++ server/insert.php
@@ -1,6 +1,9 @@
 <?php require 'init.php';?>
 <?php require 'select.php';?>
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 // Creating dummy data for test
 $con = createCon();
 if (!$con) {
@@ -10,21 +13,21 @@ if (!$con) {
 };
 
 selectDatabase($con, 'test');
-$orderId = 001;
-
-$firstName = "Wilfredo";
-
-$lastName = "Colina";
-
-$address = "20.00";
-
-$email = "wrca26@gmail.com";
-$quantity = 45;
-$price = 45.00;
 
 
-$query = /** @lang text */
-    "
+
+function insertSupply ($connection, $id, $quantity, $size, $title){
+    $orderId = $id;
+
+    $firstName = $title;
+    $lastName = "Colina";
+    $address = $size;
+    $email = "wrca26@gmail.com";
+    $q = $quantity;
+    $price = 45.00;
+
+    $query = /** @lang text */
+        "
 INSERT INTO `orders`(
     `orderId`,
     `firstName`,
@@ -40,12 +43,10 @@ VALUES(
     '$lastName',
     '$address',
     '$email',
-    '$quantity',
+    '$q',
     '$price'
 );
 ";
-
-function insertSupply ($connection, $query ){
 
     if(mysqli_query($connection,$query )){
 
@@ -58,7 +59,21 @@ function insertSupply ($connection, $query ){
 
 //insertSupply($con, $query);
 
-if (isset($_POST)){
-    header('Content-type: text/javascript');
-    echo json_encode($_POST, JSON_PRETTY_PRINT);
+if (isset($_POST['test'])){
+//    header('Content-type: text/json');
+//    $arr = json_encode($_POST,true);
+//   echo $arr;
+
+    //Decode to conver from string json to array and iterate
+   foreach (json_decode($_POST['test'],true) as $key => $val){
+       //each value equals an associate array with the values of each records
+
+       insertSupply($con,$val['uid'], $val['quantity'], $val['size'], $val['id']);
+
+//
+//       }
+       echo '<br/>';
+    }
+
+
 }
